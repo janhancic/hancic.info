014d16db7e37018056abb32cfc87531aa4a16213
Jan Hancic <jan.hancic@gmail.com>
2012-11-04 11:39:37 +0100

    split bits into php files, for easier maintnance

---
+++ footer.php
@@ -0,0 +1,8 @@
+		</div>
+		<footer class="container">
+			<small>Copyright Jan Hančič, powered by <a href="#">WordPress</a>, design built on top of <a href="#">Bootstrap</a>.</small>
+		</footer>
+		<script src="http://code.jquery.com/jquery-latest.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+	</body>
+</html>
---
+++ sidebar.php
@@ -0,0 +1,33 @@
+<aside class="span4">
+	<div class="media">
+		<a class="pull-left" href="/images/about/jan-hancic.jpg" target="_blank">
+			<img class="media-object" src="/images/about/jan-hancic-thumb.jpg" alt="Jan Hančič" style="width: 100px" />
+		</a>
+		<div class="media-body">
+			<p class="muted">
+				<small>Hi! I'm <strong>Jan Hančič</strong> and this is my blog. I'm a <strong>freelance developer</strong>. Take a look at my <a href="#">portfolio</a> or read more <a href="#">about me</a>.</small>
+			</p>
+			<ul class="nav nav-list">
+				<li class="nav-header">My profiles</li>
+				<li><a href="https://twitter.com/janhancic" target="_blank">Twitter</a></li>
+				<li><a href="https://www.linkedin.com/in/janhancic" target="_blank">LinkedIn</a></li>
+				<li><a href="https://github.com/janhancic" target="_blank">GitHub</a></li>
+				<li><a href="http://stackoverflow.com/users/185527/jan-hancic" target="_blank">Stack Overflow</a></li>
+				<li><a href="http://news.ycombinator.com/user?id=CWIZO" target="_blank">Hacker News</a></li>
+			</ul>
+			<div id="SidebarSearch">
+				<form class="form-search">
+					<input type="text" class="input-big search-query" placeholder="Search...">
+				</form>
+			</div>
+			<ul class="nav nav-list">
+				<li class="nav-header">Archive</li>
+				<li class="active2"><a href="#">September 2012</a></li>
+				<li><a href="#">August 2012</a></li>
+			</ul>
+			<div style="margin-top: 10px;">
+				<iframe src="http://stackoverflow.com/users/flair/185527.html?theme=clean" marginwidth="0" marginheight="0" frameborder="0" scrolling="no" width="210" height="60"></iframe>
+			</div>
+		</div>
+	</div>
+</aside>
