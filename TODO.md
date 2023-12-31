769fe6a6eedc00b104feae427d907b6ab59703f1
jan <jan@thoughtmachine.net>
2023-12-31 14:43:06 +0000

    Create DNS records and CloudFlare zones

--- TODO.md
+++ TODO.md
@@ -2,14 +2,9 @@
 
 ## TODO
 - write about page
-- create CloudFlare account
-- download terraform
-    - make script for running it from local bin folder
-- create .tf for CF resources
-- configure DNS to point to CF
-- deploy
 - find a lightweight theme without any JS nonsense
 - port contents of `old` (from ZIP) into the blog
+- figure out how to get the page redirect into Terraform
 - move .terraform.tfstate to some proper remote state?
 - profit?
 
@@ -20,3 +15,9 @@
 - select hugo theme
 - build a basic hugo website with about page
 - figure out how to import the .sql dump (& static files) from WP into .md files for Hugo
+- create CloudFlare account
+- download terraform
+    - make script for running it from local bin folder
+- create .tf for CF resources
+- configure DNS to point to CF
+- deploy
--- tf/main.tf
+++ tf/main.tf
@@ -15,6 +15,11 @@ variable "cf_account_id" {
   type    = string
 }
 
+variable "domain_name" {
+  default = "hancic.info"
+  type    = string
+}
+
 resource "cloudflare_pages_project" "hancic_info_pages_project" {
   account_id        = var.cf_account_id
   name              = "hancic-info"
@@ -24,7 +29,7 @@ resource "cloudflare_pages_project" "hancic_info_pages_project" {
     type = "github"
     config {
       owner                         = "janhancic"
-      repo_name                     = "hancic.info"
+      repo_name                     = var.domain_name
       production_branch             = "master"
       pr_comments_enabled           = true
       deployments_enabled           = true
@@ -55,4 +60,95 @@ resource "cloudflare_pages_project" "hancic_info_pages_project" {
       fail_open = true
     }
   }
+}
+
+resource "cloudflare_zone" "hancic_info_zone" {
+  account_id = var.cf_account_id
+  paused     = false
+  plan       = "free"
+  type       = "full"
+  zone       = var.domain_name
+}
+
+resource "cloudflare_record" "hancic_info_record_root" {
+  zone_id = cloudflare_zone.hancic_info_zone.id
+
+  name    = var.domain_name
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "hancic-info.pages.dev"
+}
+
+resource "cloudflare_record" "hancic_info_record_www" {
+  zone_id = cloudflare_zone.hancic_info_zone.id
+
+  name    = "www.${var.domain_name}"
+  proxied = true
+  ttl     = 1
+  type    = "CNAME"
+  value   = "hancic-info.pages.dev"
+}
+
+resource "cloudflare_pages_domain" "hancic_info_domain" {
+  account_id   = var.cf_account_id
+  project_name = cloudflare_pages_project.hancic_info_pages_project.name
+  domain       = var.domain_name
+}
+
+resource "cloudflare_pages_domain" "hancic_info_www_domain" {
+  account_id   = var.cf_account_id
+  project_name = cloudflare_pages_project.hancic_info_pages_project.name
+  domain       = cloudflare_record.hancic_info_record_www.name
+}
+
+// MX records for Gmail
+resource "cloudflare_record" "hancic_info_record_email_mx_1" {
+  name     = var.domain_name
+  priority = 1
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "aspmx.l.google.com"
+  zone_id  = cloudflare_zone.hancic_info_zone.id
+}
+
+resource "cloudflare_record" "hancic_info_record_email_mx_2" {
+  name     = var.domain_name
+  priority = 5
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "alt2.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.hancic_info_zone.id
+}
+
+resource "cloudflare_record" "hancic_info_record_email_mx_3" {
+  name     = var.domain_name
+  priority = 5
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "alt1.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.hancic_info_zone.id
+}
+
+resource "cloudflare_record" "hancic_info_record_email_mx_4" {
+  name     = var.domain_name
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "alt4.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.hancic_info_zone.id
+}
+
+resource "cloudflare_record" "hancic_info_record_email_mx_5" {
+  name     = var.domain_name
+  priority = 10
+  proxied  = false
+  ttl      = 1
+  type     = "MX"
+  value    = "alt3.aspmx.l.google.com"
+  zone_id  = cloudflare_zone.hancic_info_zone.id
 }
